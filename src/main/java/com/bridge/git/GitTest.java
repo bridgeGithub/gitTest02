d9cc2491a3841abd2a023088756442b1ad67ac8e
bridge <bridge@xiaopin.com>
2024-01-10 16:17:22 +0800

    dev commit2222

--- src/main/java/com/bridge/git/GitTest.java
+++ src/main/java/com/bridge/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println(1111);
         System.out.println(2222);
         System.out.println(3333333);
+        System.out.println("dev update");
 
     }
 }
